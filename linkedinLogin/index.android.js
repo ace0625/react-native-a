927406bb3d717ca2962bd132c2824b97148cc03c
Dan Hyunchan Kim <hckim0625@gmail.com>
2016-08-19 12:29:24 +0900

    first commit

---
+++ linkedinLogin/index.android.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const React = require('react-native');
+const { AppRegistry } = React;
+const Example = require('./Example');
+
+AppRegistry.registerComponent('RNLinkedinLoginExample', () => Example);
